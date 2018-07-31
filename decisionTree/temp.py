4dbfe7776829036aa15da7b81398af8b434c0c47
Kaustubh Lall <klall@ucsd.edu>
2018-07-31 11:38:46 -0700

    added charge and code for binarizing

---
+++ decisionTree/temp.py
@@ -0,0 +1,13 @@
+def convertCharge(fname):
+    f = open(fname, 'r')
+    s = ''
+    for line in f:
+        tokens = line.split(",")
+        if tokens[6] == 'anion': tokens[6] = 0
+        elif tokens[6] == 'cation': tokens[6] = 1
+        tokens = [str(x) for x in tokens]
+        s += ','.join(tokens) + '\n'
+    g = open("newfile", 'w')
+    g.write(s)
+
+convertCharge('oat1oct1.csv')
